81cc60ad9e1cfd07bb7875780e2b37858fa480cf
CovaM <cova.millan@gmail.com>
2023-12-15 13:11:05 +0100

    Add global styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './reset.scss';
+import './global.scss';
 
 ReactDOM.createRoot(document.querySelector('.root')!).render(
   <React.StrictMode>
