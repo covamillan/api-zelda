ce09b4d489ef6e4e25638aede5145bbf2819ac68
CovaM <cova.millan@gmail.com>
2023-12-15 14:48:13 +0100

    Add default model

---
+++ src/models/default.model.ts
@@ -0,0 +1,11 @@
+export type Default = {
+  id: number;
+  category: 'creatures' | 'equipment' | 'materials' | 'monsters' | 'treasure';
+  name: string;
+  description: string;
+  image: string;
+  cooking_effect?: string;
+  properties?: { attack: number; defense: number };
+  hearts_recovered?: number;
+  drops?: string[];
+};
