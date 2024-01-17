b69fa68356adfc8e78a0a537b805bf930dc92255
CovaM <cova.millan@gmail.com>
2024-01-17 13:11:09 +0100

    Add redux

--- src/redux/items.slice.ts
+++ src/redux/items.slice.ts
@@ -1,6 +1,5 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { Item } from '../models/item.model';
-import { getItemsAsync } from './categories.thunks';
 
 export type ItemsState = {
   items: Item[];
@@ -12,30 +11,15 @@ const initialState: ItemsState = {
   itemsState: 'idle',
 };
 const itemsSlice = createSlice({
-  initialState: initialState,
   name: 'items',
-  reducers: {
-    deleteItem: (state, action: PayloadAction<Item['id']>) => {
-      state.items = state.items.filter((item) => item.id !== action.payload);
-    },
-  },
+  initialState: initialState,
 
-  extraReducers: (builder) => {
-    builder.addCase(
-      getItemsAsync.fulfilled,
-      (state, { payload }: { payload: Item[] }) => {
-        state.itemsState = 'loaded';
-        state.items = payload;
-      }
-    );
-    builder.addCase(getItemsAsync.pending, (state) => {
-      state.itemsState = 'idle';
-    });
-    builder.addCase(getItemsAsync.rejected, (state) => {
-      state.itemsState = 'error';
-    });
+  reducers: {
+    getItems(state, action: PayloadAction<Item[]>) {
+      state.items = action.payload;
+    },
   },
 });
 
-export const itemsActions = itemsSlice.actions;
+export const { getItems } = itemsSlice.actions;
 export default itemsSlice.reducer;
